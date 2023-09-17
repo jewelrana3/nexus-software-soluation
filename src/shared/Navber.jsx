9b8ca8a79d370baea37f848dc38984ef1cf81c69
Jewel Rana <juyelrana7752@gmail.com>
2023-09-17 20:57:16 +0600

    last

--- src/shared/Navber.jsx
+++ src/shared/Navber.jsx
@@ -21,19 +21,20 @@ const Navber = () => {
             <li>Read</li>
             <li>Stores</li>
             <li>Facial Appointments</li>
-
-            <div className='lg:flex ml-96 m-0'>
-
+            <div className='flex ml-20'>
+                <li>Log in</li>
+                <li>Cabinet</li>
+                <li>Cart</li>
             </div>
 
 
         </>
 
     return (
-        <div className="navbar sm:bg-slate-600 text-black z-10 w-full lg:px-6">
+        <div className="navbar  sm:bg-slate-300 text-black z-10 w-full lg:px-6">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div className='flex items-center  gap-20 lg:hidden'>
+                    <div className='flex items-center man gap-14 lg:hidden'>
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
@@ -42,7 +43,7 @@ const Navber = () => {
                         <p className='text-white'><GiSelfLove /></p>
                         <p className='text-xl text-white'>Aesob</p>
                     </div>
-                    <ul tabIndex={0} className="menu -mr-30  menu-sm dropdown-content mt-1 shadow flex-none">
+                    <ul tabIndex={0} className="menu -mr-30  menu-sm dropdown-content mt-1 bg-lime-950 text-white p-20 flex-none">
                         {navItem}
                     </ul>
                 </div>
